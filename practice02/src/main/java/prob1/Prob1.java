acb24c75dcc34b80c44f1531d64de69be724e1cb
tlckd <tlckd8581@gmail.com>
2022-04-25 22:36:40 +0900

    연습문제2 Prob1 문제 해결

--- practice02/src/main/java/prob1/Prob1.java
+++ practice02/src/main/java/prob1/Prob1.java
@@ -11,8 +11,30 @@ public class Prob1 {
 		 * 50원 동전, 10원 동전, 1원 동전 각 몇 개로 변환 되는지 작성하시오.
 		 */
 		Scanner scanner = new Scanner(System.in); 
-		System.out.print("금액 : 깃 공부중..");
+		System.out.print("금액 : ");
 		int count =scanner.nextInt();
+		System.out.println();
+		
+		int won50000=0,won10000=0, won5000=0, won1000=0 ;
+		int won500=0,won100=0,won50=0,won10=0,won1=0;
+		
+		int[] typeOfWon = {50000,10000,5000,1000,500,100,50,10,5,1};
+		int[] wonCount = new int[10];
+		
+		
+		for(int i=0; i<typeOfWon.length;i++) {
+			if(count>=typeOfWon[i]) {
+				wonCount[i]=count/typeOfWon[i];
+				count = count % typeOfWon[i];
+			}
+		}
+		
+		
+		for(int i=0; i<typeOfWon.length;i++) {
+			System.out.println(typeOfWon[i] + "원 : " + wonCount[i] + "개");
+		}
+		
+		
 		
 	}
 
