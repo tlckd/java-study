23b37ad801108c68ebb2fea31da471dcfd86416d
tlckd <tlckd8581@gmail.com>
2022-04-25 18:25:39 +0900

    practice02  prob1 패키지 및 자바파일 생성

---
+++ practice02/src/main/java/prob1/prob1.java
@@ -0,0 +1,19 @@
+package prob1;
+
+import java.util.Scanner;
+
+public class prob1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 * // //키보드에서 정수로 된 돈의 액수를 입력 받아 오만 원권, 만원 권, 오천 원권, 천원 권, // 500원 동전, 100원 동전,
+		 * 50원 동전, 10원 동전, 1원 동전 각 몇 개로 변환 되는지 작성하시오.
+		 */
+		Scanner scanner = new Scanner(System.in); 
+		System.out.print("금액 : ");
+		int count =scanner.nextInt();
+		
+	}
+
+}
