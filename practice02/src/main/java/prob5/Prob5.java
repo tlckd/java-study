9f48825be991874dc2aa79d236001f9181ada935
tlckd <tlckd8581@gmail.com>
2022-04-26 18:25:16 +0900

    5번 문제 업로드..

--- practice02/src/main/java/prob5/Prob5.java
+++ practice02/src/main/java/prob5/Prob5.java
@@ -12,22 +12,51 @@ public class Prob5 {
 		while( true ) {
 			
 			/* 게임 작성 */
-
+			int input=0;
+			int count=1;
+			
 			// 정답 램덤하게 만들기
 			Random random = new Random();
 			int correctNumber = random.nextInt( 100 ) + 1;
 			System.out.println(correctNumber);
-
 			
-			//새 게임 여부 확인하기
-			System.out.print( "다시 하겠습니까(y/n)>>" );
-			String answer = scanner.next();
-			if( "y".equals( answer ) == false ) {
-				break;
+			System.out.println("수를결정하였습니다. 맞추어보세요");
+			System.out.println("1-100");
+			
+			while(true) {
+				System.out.print(count + ">>");
+				input=scanner.nextInt();
+				
+				if (correctNumber == input) {
+
+					System.out.println("맞았습니다.");
+					count=0;
+					// 새 게임 여부 확인하기
+					System.out.print("다시 하겠습니까(y/n)>>");
+					String answer = scanner.next();
+					if ("y".equals(answer) == false) {
+						break;
+					}
+					correctNumber = random.nextInt( 100 ) + 1;
+					System.out.println(correctNumber);
+					System.out.println("수를결정하였습니다. 맞추어보세요");
+					System.out.println("1-100");
+					
+					
+				}
+				else if(input>correctNumber) {
+					System.out.println("더 낮게");
+				}else {
+					System.out.println("더 높게");
+				}
+				
+				count++;
 			}
+			
+	
 		}
 		
-		scanner.close();
+		//scanner.close();
 	}
 
 }
