543d60886b97a5239ac29bc9002fc2da36b0bfe9
tlckd <tlckd8581@gmail.com>
2022-04-28 16:52:02 +0900

    tv 문제 해결

--- chapter03/src/main/java/tv/TV.java
+++ chapter03/src/main/java/tv/TV.java
@@ -6,24 +6,60 @@ public class TV {
 	private int volume;  //0~100 rotation
 	private boolean power;  
 	
-	
 	public TV() {
-		
+		this(7,20,false);
+
+	}
+	public TV(int channe, int volume, boolean power) {
+		this.channe=channe;
+		this.volume=volume;
+		this.power=power;
 	}
 	
 	public void status() {
 		// TODO Auto-generated method stub
-		System.out.println("TV[channe="+7+", volume="+ volume + ", power= " + (power ? "on" : "off" ) + "]" );
+		System.out.println("TV[channe="+channe+", volume="+ volume + ", power= " + (power ? "on" : "off" ) + "]" );
 		 
 	}
 	
-	public void volum(boolean up) {
-		volum(volume + (up?1 :-1));
+	public void volume(boolean up) {
+		volume(volume + (up?1 :-1));		
+	}
+	
+	public void volume(int volume) {		
+		if(volume>100) {
+			this.volume=volume%100;
+		}else if(volume<0) {
+			this.volume=100;
+		}else{
+		this.volume = volume;
+		}
 		
 	}
-	public void volum(int volum) {
-		this.volume = volume;
+
+	
+	public void power(boolean on) {
+		this.power=on;		
 	}
+
+	
+	public void channel(boolean up) {
+		channel(channe + (up?1 :-1));
+	}
+	
+	public void channel(int channe) {
+		if(channe>255) {
+			this.channe=0;
+		}else if(channe<0) {
+			this.channe=100;
+		}else{
+		this.channe = channe;
+		}
+	}
+	
+	
+
+	
 	
 	
 	
