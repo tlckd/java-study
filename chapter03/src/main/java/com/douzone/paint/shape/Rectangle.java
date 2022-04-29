23b286c62d50e9f3b29ead36de1d2197c930c84e
tlckd <tlckd8581@gmail.com>
2022-04-29 10:23:25 +0900

    package

--- chapter03/src/main/java/com/douzone/paint/shape/Rectangle.java
+++ chapter03/src/main/java/com/douzone/paint/shape/Rectangle.java
@@ -1,15 +1,16 @@
-package paint;
+package com.douzone.paint.shape;
 
-public class Triangle extends Shape {
-
+public class Rectangle extends Shape {
 	private int x1, y1;
 	private int x2, y2;
 	private int x3, y3;
+	private int x4, y4;
 	
 	@Override
 	public void draw() {
-		System.out.println("삼각형을 그렸습니다.");
+		System.out.println("사각형을 그렸습니다.");
 	}
+	
 	public int getX1() {
 		return x1;
 	}
@@ -46,8 +47,18 @@ public class Triangle extends Shape {
 	public void setY3(int y3) {
 		this.y3 = y3;
 	}
+	public int getX4() {
+		return x4;
+	}
+	public void setX4(int x4) {
+		this.x4 = x4;
+	}
+	public int getY4() {
+		return y4;
+	}
+	public void setY4(int y4) {
+		this.y4 = y4;
+	}
 	
 	
-	
-
 }
