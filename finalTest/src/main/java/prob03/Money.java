4758cc18a624a9c11d3a31b07b72392054d6e345
tlckd <tlckd8581@gmail.com>
2022-05-02 18:23:10 +0900

    파이널 문제 3번 푸는중..

--- finalTest/src/main/java/prob03/Money.java
+++ finalTest/src/main/java/prob03/Money.java
@@ -2,6 +2,43 @@ package prob03;
 
 public class Money {
 	private int amount;
-	
+
 	/* 코드 작성 */
+	
+	public Money(int amount) {
+		this.amount = amount;
+	}
+	
+	public Money add(Money money) {
+		int result = amount+money.getAmount();
+		Money mon = new Money(result);
+		return mon;
+	}
+	
+	public Money minus(Money money) {
+		int result = amount-money.getAmount();
+		Money mon = new Money(result);
+		return mon;
+	}
+	
+	public Money multiply(Money money) {
+		int result = amount*money.getAmount();
+		Money mon = new Money(result);
+		return mon;
+	}
+	
+	public Money devide(Money money) {
+		int result = amount/money.getAmount();
+		Money mon = new Money(result);
+		return mon;
+	}
+	
+	public int getAmount() {
+		return amount;
+	}
+
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+
 }
